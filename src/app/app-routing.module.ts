d8c661d71eed79909fd1c32c1acb738a238a395d
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2022-11-01 15:33:10 +0200

    Added routes and main modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./views/home/home.module').then((m) => m.HomeModule)
+  },
+  {
+    path: 'cart',
+    loadChildren: () => import('./views/cart/cart.module').then((m) => m.CartModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path      : '**',
+    redirectTo: 'home'
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
