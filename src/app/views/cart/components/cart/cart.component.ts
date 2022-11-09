511f6353330e780e91c9726c09482bcd3476e303
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2022-11-09 14:55:24 +0200

    Implemented cart page

--- src/app/views/cart/components/cart/cart.component.ts
+++ src/app/views/cart/components/cart/cart.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductInterface } from 'src/app/shared/interfaces/product.interface';
+import { Store } from '@ngrx/store';
+import { Product } from './../../../../shared/models/product.model';
+import { AppState } from './../../../../app.state';
+import * as ProductActions from './../../../../shared/actions/product.action';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.scss']
+  styleUrls: ['./cart.component.scss'],
 })
 export class CartComponent implements OnInit {
-  products: ProductInterface[] = [];
+  selectedProducts: Product[] = [];
+  totalSum: number = 0;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
+    this.getSelectedProducts();
   }
 
+  getSelectedProducts() {
+    this.store.subscribe((data: any) => {
+      this.selectedProducts = Object.values(data)[0] as Product[];
+    });
+  }
+
+  removeProduct(index: number) {
+    this.store.dispatch(new ProductActions.RemoveProduct(index));
+  }
+
+  removeAllProducts() {
+    this.store.dispatch(new ProductActions.RemoveAll());
+  }
 }
