b0ae70529dbe2b27f3970b62bd36ef247bc431f6
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2022-11-11 19:23:38 +0200

    Improved logic of home page

--- src/app/views/home/components/home/home.component.ts
+++ src/app/views/home/components/home/home.component.ts
@@ -4,9 +4,13 @@ import { SortEnum } from 'src/app/shared/enums/sort.enum';
 import { ProductLimitEnum } from 'src/app/shared/enums/product-limit.enum';
 import { ProductInterface } from 'src/app/shared/interfaces/product.interface';
 import { Store } from '@ngrx/store';
-import { Product } from './../../../../shared/models/product.model';
+import { Product } from '../../../../shared/store/models/product.model';
 import { AppState } from './../../../../app.state';
-import * as ProductActions from './../../../../shared/actions/product.action';
+import * as ProductActions from '../../../../shared/store/actions/products.action';
+import { selectFeatureProducts } from '../../../../shared/store/selectors/products.selector';
+import { first } from 'rxjs';
+import { selectFeatureSortedProducts } from '../../../../shared/store/selectors/sorted-products.selector';
+import * as SortedProductActions from '../../../../shared/store/actions/sorted-products.action';
 
 @Component({
   selector: 'app-home',
@@ -39,6 +43,7 @@ export class HomeComponent implements OnInit {
 
   getProducts() {
     this.productService.getAllProducts().subscribe((data) => {
+      this.store.dispatch(ProductActions.insert_products({ payload: data }));
       this.products = data;
     });
   }
@@ -46,19 +51,62 @@ export class HomeComponent implements OnInit {
   limitProducts(productLimit: string) {
     this.productService.getLimitedProducts(productLimit).subscribe((data) => {
       this.products = data;
+      this.store.dispatch(ProductActions.insert_products({ payload: data }));
+      this.store.dispatch(
+        SortedProductActions.insert_sorted_products({ payload: [] })
+      );
     });
   }
 
   sortProducts(typeOfSort: string) {
-    this.productService.getSortedProducts(typeOfSort).subscribe((data) => {
-      this.products = data;
-    });
+    this.store
+      .select(selectFeatureSortedProducts)
+      .pipe(first())
+      .subscribe((data) => {
+        if (data.length === 0) {
+          this.store
+            .select(selectFeatureProducts)
+            .pipe(first())
+            .subscribe((products) => {
+              this.sort(products, typeOfSort);
+            });
+        } else {
+          this.sort(data, typeOfSort);
+        }
+      });
+  }
+
+  sort(data: Product[], typeOfSort: string) {
+    this.products = [...data];
+    if (typeOfSort === 'asc') {
+      this.products.sort((p1, p2) => {
+        return p1.price - p2.price;
+      });
+    } else if (typeOfSort === 'desc') {
+      this.products.sort((p1, p2) =>
+        p1.price < p2.price ? 1 : p1.price > p2.price ? -1 : 0
+      );
+    }
   }
 
   getProductsbyCategory(category: string) {
-    this.productService.getProductsByCategory(category).subscribe((data) => {
-      this.products = data;
-    });
+    this.store
+      .select(selectFeatureProducts)
+      .pipe(first())
+      .subscribe((data) => {
+        let productsbyCategory: ProductInterface[] = [];
+        data.map((el) => {
+          if (el.category === category) {
+            productsbyCategory.push(el);
+          }
+        });
+        this.products = productsbyCategory;
+        this.store.dispatch(
+          SortedProductActions.insert_sorted_products({
+            payload: this.products,
+          })
+        );
+      });
   }
 
   displayOneItemInARow() {
