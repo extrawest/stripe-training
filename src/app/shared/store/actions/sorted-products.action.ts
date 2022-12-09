cc422c61cf93e59dbfcadde232515fb9c04d999e
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2022-12-09 21:41:18 +0200

    Removed unneeded files

--- src/app/shared/store/actions/sorted-products.action.ts
+++
@@ -1,7 +0,0 @@
-import { createAction, props } from '@ngrx/store';
-import { Product } from '../models/product.model';
-
-export const insert_sorted_products = createAction(
-  '[SORTED_PRODUCTS] insert',
-  props<{ payload: Product[] }>()
-);
