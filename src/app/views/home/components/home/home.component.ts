6b81da4a4c8a6b0d7d2385ad818247061841a2de
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2022-12-13 12:34:45 +0200

    Made it possible to unsubscribe and changed some types of variables

--- src/app/views/home/components/home/home.component.ts
+++ src/app/views/home/components/home/home.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { ProductService } from 'src/app/shared/services/product/product.service';
 import { ProductInterface } from 'src/app/shared/interfaces/product.interface';
 import { Store } from '@ngrx/store';
-import { Product } from '../../../../shared/store/products/models/product.model';
 import { AppState } from './../../../../app.state';
 import * as ProductActions from '../../../../shared/store/products/actions/products.action';
 import { selectProducts } from '../../../../shared/store/products/selectors/products.selector';
 import { SpanTypesEnum } from './../../../../shared/enums/span-types.enum';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { SortEnum } from 'src/app/shared/enums/sort.enum';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnDestroy {
   categories: string[] = [];
   sortTypes: string[] = ['desc', 'asc'];
   productLimits: string[] = ['3', '5', '7'];
@@ -22,20 +24,27 @@ export class HomeComponent implements OnInit {
   spanTypes = SpanTypesEnum;
   category: string;
   limit: string;
-  sortType: string;
+  sortType: SortEnum;
+  componentDestroyed$: Subject<void> = new Subject();
   constructor(
     private productService: ProductService,
     private store: Store<AppState>
   ) {}
 
   ngOnInit(): void {
-    this.productService.getCategories().subscribe((data) => {
-      data.map((el: string) => this.categories.push(el));
-    });
-    this.store.dispatch(ProductActions.getAllProducts());
-    this.store.select(selectProducts).subscribe((data) => {
-      this.products = data;
-    });
+    this.productService
+      .getCategories()
+      .pipe(takeUntil(this.componentDestroyed$))
+      .subscribe((data) => {
+        data.map((el: string) => this.categories.push(el));
+      });
+    this.store.dispatch(ProductActions.getSelectedProducts({}));
+    this.store
+      .select(selectProducts)
+      .pipe(takeUntil(this.componentDestroyed$))
+      .subscribe((data) => {
+        this.products = data;
+      });
   }
 
   getSelectedProductsByCategory(category: string) {
@@ -49,36 +58,36 @@ export class HomeComponent implements OnInit {
   }
 
   getSelectedProductsBySortType(sortType: string) {
-    this.sortType = sortType;
+    if (sortType === SortEnum.ASCEND) {
+      this.sortType = SortEnum.ASCEND;
+    } else {
+      this.sortType = SortEnum.DESCEND;
+    }
     this.manageProducts();
   }
 
   manageProducts() {
-    if (this.category) {
-      this.store.dispatch(
-        ProductActions.getSelectedProducts({
-          category: this.category,
-          sortType: this.sortType,
-          limit: this.limit,
-        })
-      );
-      this.store.select(selectProducts).subscribe((data) => {
+    this.store.dispatch(
+      ProductActions.getSelectedProducts({
+        category: this.category,
+        sortType: this.sortType,
+        limit: this.limit,
+      })
+    );
+    this.store
+      .select(selectProducts)
+      .pipe(takeUntil(this.componentDestroyed$))
+      .subscribe((data) => {
         this.products = data;
       });
-    } else {
-      this.store.dispatch(
-        ProductActions.getSelectedProductsBySortTypeAndLimit({
-          sortType: this.sortType,
-          limit: this.limit,
-        })
-      );
-      this.store.select(selectProducts).subscribe((data) => {
-        this.products = data;
-      });
-    }
   }
 
   setDisplayItemsInARow(span: SpanTypesEnum): void {
     this.nzSpan = span;
+  }
+
+  ngOnDestroy(): void {
+    this.componentDestroyed$.next();
+    this.componentDestroyed$.complete();
   }
 }
