fe1ea6c6271a086087cd49684334c798d0ba990a
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2022-12-13 12:30:51 +0200

    Removed unneeded code

--- src/app/shared/store/products/reducers/products.reducer.ts
+++ src/app/shared/store/products/reducers/products.reducer.ts
@@ -1,10 +1,6 @@
 import { Product } from '../models/product.model';
 import { createReducer, on } from '@ngrx/store';
-import {
-  getAllProductsSuccess,
-  getSelectedProductsSuccess,
-  getSelectedProductsBySortTypeAndLimitSuccess,
-} from '../actions/products.action';
+import { getSelectedProductsSuccess } from '../actions/products.action';
 
 export const initialState: Product[] = [];
 
@@ -12,12 +8,6 @@ export const ProductsReducer = createReducer(
   initialState,
 
   on(getSelectedProductsSuccess, (_, result) => {
-    return result.response;
-  }),
-  on(getSelectedProductsBySortTypeAndLimitSuccess, (_, result) => {
-    return result.response;
-  }),
-  on(getAllProductsSuccess, (_, result) => {
     return result.response;
   })
 );
