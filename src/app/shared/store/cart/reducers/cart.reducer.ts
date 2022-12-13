5304141ccd35fba32a53e51096d815d0c9f80ab7
Bohdan.Horodyskyi <bohdanhorodyskyi@gmail.com>
2022-12-13 12:29:45 +0200

    Changed methods on plus and on minus item

--- src/app/shared/store/cart/reducers/cart.reducer.ts
+++ src/app/shared/store/cart/reducers/cart.reducer.ts
@@ -23,44 +23,52 @@ export const CartReducer = createReducer(
   }),
 
   on(plusItem, (state, action) => {
-    let cart: Cart[] = [...state];
-    let updatedState = [];
-    if (cart.length > 0) {
-      const item = cart.find(
-        (el) => el.product.id === action.payload.product.id
-      )!;
-      if (item) {
-        const itemIndex = cart.indexOf(item);
-        cart[itemIndex] = {
-          product: item.product,
-          quantity: item.quantity + 1,
-          totalSum: +(item.totalSum + item.product.price).toFixed(2),
-        };
-        updatedState = cart;
-      } else {
-        updatedState = [...cart, action.payload];
-      }
-    } else {
-      updatedState = [...state, action.payload];
+    const clonedState: Cart[] = [...state];
+
+    if (!clonedState.length) {
+      clonedState.push(action.payload);
+
+      return clonedState;
     }
-    return updatedState;
+
+    const itemIndex = clonedState.findIndex(
+      (cart) => cart.product.id === action.payload.product.id
+    );
+
+    if (itemIndex === -1) {
+      clonedState.push(action.payload);
+
+      return clonedState;
+    }
+
+    clonedState[itemIndex] = {
+      ...clonedState[itemIndex],
+      quantity: clonedState[itemIndex].quantity + 1,
+      totalSum: +(
+        clonedState[itemIndex].totalSum + clonedState[itemIndex].product.price
+      ).toFixed(2),
+    };
+
+    return clonedState;
   }),
 
   on(minusItem, (state, action) => {
-    let cart = [...state];
-    const item: Cart = cart.find(
-      (el) => el.product.id === action.payload.product.id
-    )!;
-    const itemIndex = cart.indexOf(item);
-    if (item.quantity > 1) {
-      cart[itemIndex] = {
-        product: item.product,
-        quantity: item.quantity - 1,
-        totalSum: +(item.totalSum - item.product.price).toFixed(2),
+    const clonedState: Cart[] = [...state];
+    const itemIndex = clonedState.findIndex(
+      (cart) => cart.product.id === action.payload.product.id
+    );
+    if (clonedState[itemIndex].quantity > 1) {
+      clonedState[itemIndex] = {
+        ...clonedState[itemIndex],
+        quantity: clonedState[itemIndex].quantity - 1,
+        totalSum: +(
+          clonedState[itemIndex].totalSum - clonedState[itemIndex].product.price
+        ).toFixed(2),
       };
-    } else if (item.quantity === 1) {
-      cart.splice(itemIndex, 1);
     }
-    return cart;
+    if (clonedState[itemIndex].quantity === 1) {
+      clonedState.splice(itemIndex, 1);
+    }
+    return clonedState;
   })
 );
